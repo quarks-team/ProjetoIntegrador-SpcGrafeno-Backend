bb56dd039cb8590d63a0a3fbecd2fa51fe95a7d5
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2024-09-29 22:16:12 -0300

    Update configuration.ts

--- src/infra/config/configuration.ts
+++ src/infra/config/configuration.ts
@@ -1,5 +1,5 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 80,
+  port: parseInt(process.env.PORT, 10) || 3000,
   storage: {
     url: process.env.STORAGE_URL,
     bucket: process.env.BUCKET,
